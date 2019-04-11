0cbf00a07d174f0553899772e55ad9d9b7d3e059
Yanilda <synac1@users.noreply.github.com>
2019-04-11 12:45:06 -0400

    Create Human.java

---
+++ lifeProject/Human.java
@@ -0,0 +1,44 @@
+/*
+ * Author: Yanilda
+ * desc: 
+ * created
+ */
+public class Human {
+	//instance Variables
+	public int age;
+	public int food;
+	public double wealth;
+	public double hunger;
+	public boolean hasVehicle;
+	public String name;
+	
+	
+	public void eat() {
+		if(food>0)
+			hunger--;
+		else
+			System.out.println("No enough please buy food");
+	}
+	public void grow() {
+		age++;
+	}
+	public void work() {
+		if(hunger<5)
+			wealth += 10.5;
+		else
+			System.out.println("Too hungry to work");
+	}
+	public void buyFood() {
+		if(wealth>5) {
+			wealth -= 4.5;
+			food++;
+		}
+		else {
+			System.out.println("No enough money, please work");
+		}
+		
+	}
+	
+	
+	
+}
